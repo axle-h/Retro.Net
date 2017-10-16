e6513d67d672075a10f3396077f0f83f644eb748
Alex Haslehurst <alex.haslehurst@gmail.com>
2017-10-16 20:21:10 +0100

    Shift and rotate tests

--- Retro.Net.Tests/Z80/Execute/Arithmetic8BitTests.cs
+++ Retro.Net.Tests/Z80/Execute/Arithmetic8BitTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq.Expressions;
+using Retro.Net.Tests.Util;
 using Retro.Net.Z80.Core;
 using Retro.Net.Z80.Core.Decode;
 using Shouldly;
@@ -28,7 +29,59 @@ namespace Retro.Net.Tests.Z80.Execute
         [Fact] public void Increment() => TestCallAssign(OpCode.Increment, (alu, o0) => alu.Increment(o0));
 
         [Fact] public void Decrement() => TestCallAssign(OpCode.Decrement, (alu, o0) => alu.Decrement(o0));
-        
+
+        [Fact] public void RotateLeftWithCarry() => TestCallAssign(OpCode.RotateLeftWithCarry, (alu, o) => alu.RotateLeftWithCarry(o));
+
+        [Fact] public void RotateLeftWithCarry_AlternativeFlags() => TestCallAssign(OpCode.RotateLeftWithCarry, (alu, o) => alu.RotateLeftWithCarry(o), true);
+
+        [Fact] public void RotateLeft() => TestCallAssign(OpCode.RotateLeft, (alu, o) => alu.RotateLeft(o));
+
+        [Fact] public void RotateLeft_AlternativeFlags() => TestCallAssign(OpCode.RotateLeft, (alu, o) => alu.RotateLeft(o), true);
+
+        [Fact] public void RotateRightWithCarry() => TestCallAssign(OpCode.RotateRightWithCarry, (alu, o) => alu.RotateRightWithCarry(o));
+
+        [Fact] public void RotateRightWithCarry_AlternativeFlags() => TestCallAssign(OpCode.RotateRightWithCarry, (alu, o) => alu.RotateRightWithCarry(o), true);
+
+        [Fact] public void RotateRight() => TestCallAssign(OpCode.RotateRight, (alu, o) => alu.RotateRight(o));
+
+        [Fact] public void RotateRight_AlternativeFlags() => TestCallAssign(OpCode.RotateRight, (alu, o) => alu.RotateRight(o), true);
+
+        [Fact]
+        public void RotateLeftDigit()
+        {
+            using (var fixture = new ExecuteFixture())
+            {
+                var result = RngFactory.Build<AccumulatorAndResult>()();
+                fixture.Operation.OpCode(OpCode.RotateLeftDigit);
+                fixture.With(c => c.Mmu.Setup(x => x.ReadByte(c.Registers.HL)).Returns(c.Byte).Verifiable(),
+                    c => c.Alu.Setup(x => x.RotateLeftDigit(c.InitialAccumulator.A, c.Byte)).Returns(result).Verifiable());
+                fixture.Assert(c => c.Accumulator.A.ShouldBe(result.Accumulator),
+                    c => c.Mmu.Verify(x => x.WriteByte(c.Registers.HL, result.Result)));
+            }
+        }
+
+        [Fact]
+        public void RotateRightDigit()
+        {
+            using (var fixture = new ExecuteFixture())
+            {
+                var result = RngFactory.Build<AccumulatorAndResult>()();
+                fixture.Operation.OpCode(OpCode.RotateRightDigit);
+                fixture.With(c => c.Mmu.Setup(x => x.ReadByte(c.Registers.HL)).Returns(c.Byte).Verifiable(),
+                    c => c.Alu.Setup(x => x.RotateRightDigit(c.InitialAccumulator.A, c.Byte)).Returns(result).Verifiable());
+                fixture.Assert(c => c.Accumulator.A.ShouldBe(result.Accumulator),
+                    c => c.Mmu.Verify(x => x.WriteByte(c.Registers.HL, result.Result)));
+            }
+        }
+
+        [Fact] public void ShiftLeft() => TestCallAssign(OpCode.ShiftLeft, (alu, o) => alu.ShiftLeft(o));
+
+        [Fact] public void ShiftLeftSet() => TestCallAssign(OpCode.ShiftLeftSet, (alu, o) => alu.ShiftLeftSet(o));
+
+        [Fact] public void ShiftRight() => TestCallAssign(OpCode.ShiftRight, (alu, o) => alu.ShiftRight(o));
+
+        [Fact] public void ShiftRightLogical() => TestCallAssign(OpCode.ShiftRightLogical, (alu, o) => alu.ShiftRightLogical(o));
+
         private static void TestAccumulatorAssign(OpCode op, Expression<Func<IAlu, byte, byte, byte>> f)
         {
             using (var fixture = new ExecuteFixture())
@@ -39,13 +92,18 @@ namespace Retro.Net.Tests.Z80.Execute
             }
         }
 
-        private static void TestCallAssign(OpCode op, Expression<Func<IAlu, byte, byte>> f)
+        private static void TestCallAssign(OpCode op, Expression<Func<IAlu, byte, byte>> f, bool alternativeFlags = false)
         {
             using (var fixture = new ExecuteFixture())
             {
-                fixture.Operation.OpCode(op).RandomRegister(out var o).RandomLiterals();
+                fixture.Operation.OpCode(op).RandomRegister(out var o).RandomLiterals().UseAlternativeFlagAffection(alternativeFlags);
                 fixture.With(c => c.Alu.Setup(c.Alu8Call(f, o)).Returns(c.Byte).Verifiable());
                 fixture.Assert(c => c.Operand8(o).ShouldBe(c.Byte));
+
+                if (alternativeFlags)
+                {
+                    fixture.AssertFlags(zero: false, sign: false);
+                }
             }
         }
 
